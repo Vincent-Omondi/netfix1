9a5dd3c197c4d22654a53d45be4df62944ac8389
vinomondi <vincentomondi1002@gmail.com>
2024-09-20 16:34:01 +0300

    add: error urls

--- netfix/urls.py
+++ netfix/urls.py
@@ -1,7 +1,8 @@
 # netfix/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler400, handler403, handler404, handler500
+from main.views import error_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +10,8 @@ urlpatterns = [
     path('services/', include('services.urls')),  # Include services app URLs
     path('', include('main.urls')),  # Include main app URLs
 ]
+
+handler400 = error_view
+handler403 = error_view
+handler404 = error_view
+handler500 = error_view

bea09950fa8efbc5178a27f2c8b2e42e7b70d683
Vincent-Omondi <vincentomondi251@gmail.com>
2024-09-28 12:28:43 +0300

    refactor: remove duplicate function definition

--- main/views.py
+++ main/views.py
@@ -5,26 +5,29 @@ from django.db.models import Count
 from services.models import Service, ServiceHistory
 
 
-def home(request):
-    return render(request, "main/home.html", {})
-
-
 def logout(request):
+    # Log out the user using Django's built-in logout function
     django_logout(request)
+    # Render the logout page
     return render(request, "main/logout.html")
 
 
 def home(request):
+    # Query for the 8 most requested services
     most_requested_services = Service.objects.annotate(
         request_count = Count('servicehistory')
     ).filter(request_count__gt=0).order_by('-request_count')[:8]
 
+    # Prepare the context dictionary for the template
     context = {
         'most_requested_services': most_requested_services,
     }
+    # Render the home page with the context
     return render(request, "main/home.html", context)
     
 def error_view(request, exception=None, status=None):
+    # Handle error pages with a custom template
     # Use the status code passed by Django (if it's provided)
     status_code = status if status else 500
+    # Render the error template with the status code
     return render(request, 'error.html', {'code': status_code}, status=status_code)
